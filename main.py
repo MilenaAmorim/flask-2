5af71b3f14076f8b319c4d84893e8ef2f658bdc1
MilenaAmorim1 <101368549+MilenaAmorim@users.noreply.github.com>
2022-05-26 22:57:54 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+
+from flask import Flask, render_template
+app = Flask('app')
+
+@app.route('/')
+def index():
+  return render_template('index.html')
+
+@app.route('/unifran')
+def unifran():
+  return render_template('unifran.html')
+
+  @app.route('/dashboard/Milena')
+def milena():
+  return render_template('milena.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
